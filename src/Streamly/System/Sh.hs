9e80a9c23ced971c1244e476959b47f49a7f7a0b
Harendra Kumar <harendra@composewell.com>
2021-07-14 18:07:49 +0530

    Change API names, use monomorphic signatures

--- src/Streamly/System/Sh.hs
+++ src/Streamly/System/Sh.hs
@@ -10,27 +10,24 @@
 -- and output.  The functions in this module are just convenience wrappers over
 -- "Streamly.System.Process" to run shell commands using "/bin/sh".
 --
--- >>> from = Sh.with Process.toBytes_
--- >>> via = Sh.via Process.processBytes_
+-- >>> src = srcWith Process.toBytes
+-- >>> pipe = pipeWith Process.processBytes
 -- >>> :{
---   from "echo hello"
--- & via "tr [a-z] [A-Z]"
+--   src "echo hello"
+-- & pipe "tr [a-z] [A-Z]"
 -- & Stream.fold Stdio.write
 -- :}
 -- HELLO
 --
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE RankNTypes #-}
 
 module Streamly.System.Sh
     (
-      with
-    , via
+      srcWith
+    , pipeWith
     )
 where
 
-import Data.Kind (Type)
+import Streamly.Prelude (SerialT)
 
 -- The APIs are named/designed such that we can replace the sh module with
 -- another module for bash or any other shells without requiring API name
@@ -38,35 +35,50 @@ import Data.Kind (Type)
 
 -- $setup
 -- >>> :set -XFlexibleContexts
+-- >>> :set -package streamly
+-- >>> :set -package streamly-process
 -- >>> import Data.Function ((&))
 -- >>> import qualified Streamly.Console.Stdio as Stdio
 -- >>> import qualified Streamly.Prelude as Stream
 -- >>> import qualified Streamly.System.Process as Process
+-- >>> import qualified Streamly.Unicode.Stream as Unicode
 
 -- | A modifier for stream generation process APIs to generate streams from
 -- shell scripts with "/bin/sh" as the shell interpreter.
 --
--- >>> with Process.toBytes_ "echo hello" & Stream.fold Stdio.write
+-- >>> srcWith Process.toBytes "echo hello" & Stream.fold Stdio.write
 -- hello
--- >>> with Process.toChunks_ "echo hello" & Stream.fold Stdio.writeChunks
+-- >>> srcWith Process.toChunks "echo hello" & Stream.fold Stdio.writeChunks
 -- hello
 --
--- >>> with f cmd = f "/bin/sh" ["-c", cmd]
+-- >>> srcWith f cmd = f "/bin/sh" ["-c", cmd]
 --
-with :: forall (t :: (Type -> Type) -> Type -> Type) m a.
-    (FilePath -> [String] -> t m a) -> String -> t m a
-with f cmd = f "/bin/sh" ["-c", cmd]
+srcWith :: (FilePath -> [String] -> SerialT m a) -> String -> SerialT m a
+srcWith f cmd = f "/bin/sh" ["-c", cmd]
 
 -- | A modifier for stream transformation process APIs to transform streams
--- using shell scripts with "/bin/sh" as the shell interpreter.
+-- using shell scripts srcWith "/bin/sh" as the shell interpreter.
 --
--- >>> via Process.processBytes_ "tr [a-z] [A-Z] hello" & Stream.fold Stdio.write
--- HELLO
--- >>> via Process.processChunks_ "tr [a-z] [A-Z} hello" & Stream.fold Stdio.writeChunks
--- HELLO
+-- >>> :{
+--    Stream.fromList "hello"
+--  & Unicode.encodeLatin1
+--  & pipeWith Process.processBytes "tr [a-z] [A-Z]"
+--  & Stream.fold Stdio.write
+--  :}
+--HELLO
 --
--- >>> via f cmd = f "/bin/sh" ["-c", cmd]
+-- >>> :{
+--    srcWith Process.toChunks "echo hello"
+--  & pipeWith Process.processChunks "tr [a-z] [A-Z]"
+--  & Stream.fold Stdio.writeChunks
+--  :}
+--HELLO
 --
-via :: forall (t :: (Type -> Type) -> Type -> Type) m a b.
-    (FilePath -> [String] -> t m a -> t m b) -> String -> t m a -> t m b
-via f cmd = f "/bin/sh" ["-c", cmd]
+-- >>> pipeWith f cmd = f "/bin/sh" ["-c", cmd]
+--
+pipeWith ::
+       (FilePath -> [String] -> SerialT m a -> SerialT m b)
+    -> String
+    -> SerialT m a
+    -> SerialT m b
+pipeWith f cmd = f "/bin/sh" ["-c", cmd]
